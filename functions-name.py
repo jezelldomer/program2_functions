5687af26cf6f8e328013d5a35980309f40944b2c
Jezell <jzlldomer@gmail.com>
2021-11-04 15:25:22 +0800

    print

--- functions-name.py
+++ functions-name.py
@@ -12,6 +12,10 @@ def get_address():
     _address = input("Enter your address: ")
     return _address
 
+def display(nameF, ageF, addF):
+    print(f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addF}. ")
+
 name = get_name()
 age = get_age()
 address = get_address()
+display(name, age, address)
