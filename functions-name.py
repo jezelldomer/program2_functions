7278c7e5332431a55e21bbf4044bf254421ee5d5
Jezell <jzlldomer@gmail.com>
2021-11-04 15:19:11 +0800

    name,age

---
+++ functions-name.py
@@ -0,0 +1,11 @@
+import math
+
+def get_name():
+    _name = input("Enter your name: ")
+    return _name
+
+def get_age():
+    _age = input("Enter your age: ")
+    return _age
+
+name = get_name()
