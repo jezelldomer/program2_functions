bbed8694f640ac454a76cdaffbed5c5f3c398f1a
Jezell <jzlldomer@gmail.com>
2021-11-04 15:21:28 +0800

    address

--- functions-name.py
+++ functions-name.py
@@ -8,4 +8,10 @@ def get_age():
     _age = input("Enter your age: ")
     return _age
 
+def get_address():
+    _address = input("Enter your address: ")
+    return _address
+
 name = get_name()
+age = get_age()
+address = get_address()
